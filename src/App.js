5bd8958e8ba9bfaaddbdefeb3f0e045a4e36edef
praharsht9 <praharsht9@gmail.com>
2022-11-16 17:27:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Child from './Child';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	function receiveData(data) {
+		// name is assigned in data
+		console.log(data);
+	}
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<Child getData={receiveData} />
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Child.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Child = (props) => {
+	const [name, setName] = useState();
+	function handleSubmit(e) {
+		e.preventDefault();
+		props.getData(name); // name is a parameter
+	}
+	return (
+		<div>
+			<h1>Child Page</h1>
+
+			<form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					value={name}
+					onChange={(e) => {
+						setName(e.target.value);
+					}}
+				/>
+
+				<button>Submits</button>
+			</form>
+		</div>
+	);
+};
+
+export default Child;
